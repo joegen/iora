55ec64d9b7033f38197595c123912bc37d6b6b51
Joegen Baclor <joegen@baclor.com>
2025-08-08 13:45:12 +0000

    chore(license): add MPL-2.0 copyright and license header to all source files

--- include/iora/network/webhook_server.hpp
+++ include/iora/network/webhook_server.hpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2025 Joegen Baclor
+// SPDX-License-Identifier: MPL-2.0
+//
+// This file is part of Iora, which is licensed under the Mozilla Public License 2.0.
+// See the LICENSE file or <https://www.mozilla.org/MPL/2.0/> for details.
+
 #pragma once
 #include <iostream>
 #include <stdexcept>
--- sample/microservice_example.cpp
+++ sample/microservice_example.cpp
@@ -1,4 +1,10 @@
-#include <csignal>
+// Copyright (c) 2025 Joegen Baclor
+// SPDX-License-Identifier: MPL-2.0
+//
+// This file is part of Iora, which is licensed under the Mozilla Public License 2.0.
+// See the LICENSE file or <https://www.mozilla.org/MPL/2.0/> for details.
+
+
 /// \file microservice_example.cpp
 /// \brief Example microservice using IoraService to expose a text‑summarisation API.
 /// 
@@ -34,6 +40,7 @@
 #include <atomic>
 #include <unordered_map>
 #include <mutex>
+#include <csignal>
 
 int main(int argc, char** argv)
 {
