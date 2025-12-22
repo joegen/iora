dc7154e0681993f7806c13d06bb33fe0ef371388
Joegen Baclor <joegen@baclor.com>
2025-12-22 15:21:31 +0800

    Remove CMake build artifacts from tracking

--- tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/karoo_sbc/libs/iora/tests
-# Build directory: /workspace/karoo_sbc/libs/iora/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
